533c1ff9c10ab3b2e94031d692b405185410bd71
8812k <8812k@DESKTOP-M25SL7J>
2017-08-21 21:02:55 -0700

    Merge

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -6,10 +6,14 @@ public class BubbleSort {
 	public static void main(String[] args) {
 	
 
-		int[]arr={43,32,04,56,92,11,23}; //array
+		int[]arr={43,32,04,56,92,11,23}; //array_available
+		
 		for(int i=0;i<arr.length;i++){
+			
 			for(int j=0;j<i;j++){
+				
 				if(arr[i]<arr[j]){
+					
 					int temp=arr[i];
 					arr[i]=arr[j];
 					arr[j]=temp;
@@ -17,6 +21,7 @@ public class BubbleSort {
 			}
 		}
 		System.out.println("Sorted Array :");
+		
 		for(int k=0;k<arr.length;k++)
 			System.out.print(arr[k]+" ");
 	}
